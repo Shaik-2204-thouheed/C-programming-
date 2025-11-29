0d7b2989d537c0472dc36ff31f65b9ff842369f3
Shaik-2204-thouheed <25kq1a5458@pace.ac.in>
2025-11-29 09:37:03 +0530

    Correct input variable and clean up code
    Fix variable name in scanf and remove extra lines.

--- even_odd.c
+++ even_odd.c
@@ -1,9 +1,8 @@
+#include<stdio.h>
 int main() {
     int n;
     printf("Enter an integer: ");
     scanf("%d",&a);
-    }
-
     if (n % 2 == 0){
         printf("%d is even.\n", n);
     } else  {
